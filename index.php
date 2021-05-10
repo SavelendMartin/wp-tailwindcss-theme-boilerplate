fb73215484c01c1d57af520c25b1cf17837ae610
Martin Dahlén Jansson <martin@savelend.se>
2021-05-10 12:22:42 +0200

    updates

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 
 get_header();
 
-get_template_part( 'templates/partials/example' );
+<?php if ( have_posts() ) : ?>
+    <?php
+    while ( have_posts() ) :
+        the_post();
+        ?>
+
+        <?php get_template_part( 'templates/partials/content', get_post_format() ); ?>
+
+    <?php endwhile; ?>
+
+<?php endif; ?>
 
 get_footer();
---
+++ templates/partials/content.php
@@ -0,0 +1,41 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'mb-12' ); ?>>
+
+	<header class="entry-header mb-8">
+		<?php the_title('<h1 class="entry-title text-6xl text-center font-display uppercase tracking-wider">', '</a></h1>' ); ?>
+	</header>
+
+	<?php if ( is_search() || is_archive() ) : ?>
+
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div>
+
+	<?php else : ?>
+
+		<div class="leading-relaxed">
+			<?php
+			/* entry-content */
+			/* translators: %s: Name of current post */
+			the_content(
+				sprintf(
+					__( 'Continue reading %s', 'tailpress' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				)
+			);
+
+			wp_link_pages(
+				array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'tailpress' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'tailpress' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">, </span>',
+				)
+			);
+			?>
+		</div>
+
+	<?php endif; ?>
+
+</article>
