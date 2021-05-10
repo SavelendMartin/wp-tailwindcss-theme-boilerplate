71aecd0689790b02e9ba8fe4f8397bae42347a56
Martin Dahlén Jansson <martin@savelend.se>
2021-05-10 13:05:25 +0200

    updates

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -6,6 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= wp_get_document_title() ?></title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Blinker:wght@300;400;600&display=swap" rel="stylesheet">   
+    
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
 	<?php wp_head() ?>
 </head>
