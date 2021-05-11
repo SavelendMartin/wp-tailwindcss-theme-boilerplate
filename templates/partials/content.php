72cd0c3cf66150172c956a4493ca85c94c8664ab
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 13:07:55 +0200

    Update content.php

--- templates/partials/content.php
+++ templates/partials/content.php
@@ -1,6 +1,6 @@
 <?php get_template_part( 'templates/partials/header'); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'max-w-screen-lg mx-auto py-12' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'max-w-screen-md mx-auto py-12' ); ?>>
 
 	<header class="text-4xl mb-8 font-semibold">
 		<?php the_title('<h1>', '</h1>' ); ?>
