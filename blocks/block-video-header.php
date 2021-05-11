0069a9a2a003eed21fd1e97ded2fabedd38be6d2
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 22:30:44 +0200

    updates

--- blocks/block-video-header.php
+++ blocks/block-video-header.php
@@ -6,9 +6,12 @@
 
 ?>
 
-<div class="w-full bg-cover bg-no-repeat h-64 md:h-80 bg-center relative mb-32" style="background-image: url('<?php block_field( 'image-background', true ); ?>')">
+<div class="w-full bg-cover bg-no-repeat h-64 md:h-80 bg-center relative mb-32 angled" style="background-image: url('<?php block_field( 'image-background', true ); ?>')">
+<svg class="w-full h-10 absolute bottom-0 left-0 z-10 text-offwhite" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 16 16" preserveAspectRatio="none">
+    <polygon points="16,0 16,16 0,16" fill="currentColor"/>
+</svg>
 <iframe width="640" height="360"
 src="https://www.youtube.com/embed/9-1kBMvjQIk"
-class="absolute left-1/2 transform -translate-x-1/2 top-1/3 border border-offwhite shadow-2xl">
+class="absolute left-1/2 transform -translate-x-1/2 top-2/6 border border-offwhite shadow-2xl z-20">
 </iframe>
 </div>
