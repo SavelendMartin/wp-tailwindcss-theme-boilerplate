4c0100983da5fb6c00a8a8ff50350ffd2e67fc02
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 18:29:13 +0200

    Update ipopopup.php

--- templates/partials/ipopopup.php
+++ templates/partials/ipopopup.php
@@ -26,7 +26,7 @@
         From: "opacity-100 translate-y-0 sm:scale-100"
         To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     -->
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
+        <div id="popup1" class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
                     <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -85,7 +85,7 @@
             </div>
         </div>
 
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
+        <div id="popup2" class="hidden inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
                     <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -136,7 +136,7 @@
             </div>
         </div>
 
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
+        <div id="popup3" class="hidden inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
                     <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -165,6 +165,10 @@
 </div>
 
 <script>
+var popup1 = document.getElementById('popup1');
+var popup2 = document.getElementById('popup2');
+var popup3 = document.getElementById('popup3');
+
 var country1 = document.getElementById('country1');
 var country2 = document.getElementById('country1');
 
@@ -172,7 +176,11 @@ var continueBtn = document.getElementById('continue');
 var acceptBtn = document.getElementById('accept');
 
 continueBtn.addEventListener('click', function(){
-    alert('click');
+    var country1val = country1.value;
+    var country2val = country2.value;
+
+    console.log(country1val);
+    console.log(country2val);
 });
 
 </script>
