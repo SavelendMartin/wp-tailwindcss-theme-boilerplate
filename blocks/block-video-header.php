94459837b6afecf461705f4c59bc226f5271fbd8
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 22:34:40 +0200

    Update block-video-header.php

--- blocks/block-video-header.php
+++ blocks/block-video-header.php
@@ -10,8 +10,8 @@
 <svg class="w-full h-10 absolute bottom-0 left-0 z-10 text-offwhite-400" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 16 16" preserveAspectRatio="none">
     <polygon points="16,0 16,16 0,16" fill="currentColor"/>
 </svg>
-<iframe width="640" height="360"
+<iframe
 src="https://www.youtube.com/embed/9-1kBMvjQIk"
-class="absolute left-1/2 transform -translate-x-1/2 top-1/3 border border-offwhite shadow-2xl z-20">
+class="absolute left-1/2 transform -translate-x-1/2 top-1/3 border border-offwhite shadow-2xl z-20 w-full max-w-prose">
 </iframe>
 </div>
