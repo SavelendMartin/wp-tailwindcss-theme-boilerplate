552b5881dd8828fa05c20b33463f0fb38e048f0e
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 13:32:04 +0200

    Update header.php

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -4,7 +4,7 @@
     </div>
 
     <?php echo wp_nav_menu([
-    //'menu'              => "2", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+    'menu'              => "main-menu", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
     'menu_class'        => "", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
     'menu_id'           => "", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
     'container'         => "nav", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
@@ -16,7 +16,7 @@
     'link_after'        => "", // (string) Text after the link text.
     'echo'              => "true", // (bool) Whether to echo the menu or return it. Default true.
     'depth'             => "0", // (int) How many levels of the hierarchy are to be included. 0 means all. Default 0.
-    'theme_location'    => "main", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
+    //'theme_location'    => "main", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
     'items_wrap'        => "", // (string) How the list items should be wrapped. Default is a ul with an id and class. Uses printf() format with numbered placeholders.
     ]); ?>
 </header>
