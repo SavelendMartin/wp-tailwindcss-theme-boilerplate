37c2459b1c5d72a35c7fa99313b25d7cbb888fa5
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 13:05:37 +0200

    Update header.php

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,9 +1,9 @@
 <header class="">
-    <div class="text-center bg-progressive py-2">
+    <div class="text-center bg-progressive py-4">
         SaveLend Group
     </div>
 
-    <nav class="bg-challenger">
+    <nav class="bg-challenger flex justify-center items-center py-4 px-4">
         <button>Länk 1</button>
         <button>Länk 2</button>
         <button>Länk 3</button>
