58196d28c81ebbd6e3525973a5b3c037a044d495
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 22:10:32 +0200

    Update block-video-header.php

--- blocks/block-video-header.php
+++ blocks/block-video-header.php
@@ -6,8 +6,9 @@
 
 ?>
 
-<div class="w-full bg-cover bg-no-repeat h-64 md:h-80 bg-center relative" style="background-image: url('<?php block_field( 'image-background', true ); ?>')">
-<iframe width="640" height="520"
-src="https://www.youtube.com/embed/9-1kBMvjQIk">
+<div class="w-full bg-cover bg-no-repeat h-64 md:h-80 bg-center relative mb-24" style="background-image: url('<?php block_field( 'image-background', true ); ?>')">
+<iframe width="512" height="288"
+src="https://www.youtube.com/embed/9-1kBMvjQIk"
+class="absolute left-auto right-auto top-50%">
 </iframe>
 </div>
