afd5d58367e7a1b4b24c10bd952bfe291ad63e3f
Martin Dahlén Jansson <martin@savelend.se>
2021-05-11 22:07:06 +0200

    Update block-video-header.php

--- blocks/block-video-header.php
+++ blocks/block-video-header.php
@@ -7,4 +7,7 @@
 ?>
 
 <div class="w-full bg-cover bg-no-repeat h-64 md:h-80 bg-center relative" style="background-image: url('<?php block_field( 'image-background', true ); ?>')">
+<iframe width="640" height="520"
+src="https://www.youtube.com/embed/9-1kBMvjQIk">
+</iframe>
 </div>
