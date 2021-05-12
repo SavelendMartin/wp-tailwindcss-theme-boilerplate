e4cc175de9822ea62aee364af3d354891e6b71a6
Martin Dahlén Jansson <martin@savelend.se>
2021-05-12 09:46:37 +0200

    Update scripts-and-styles.php

--- includes/scripts-and-styles.php
+++ includes/scripts-and-styles.php
@@ -10,11 +10,18 @@ add_action( 'wp_enqueue_scripts', function () {
 	wp_register_style( 'app', AssetResolver::resolve( 'css/app.css' ), [], false );
 	wp_register_script( 'app', AssetResolver::resolve( 'js/app.js' ), [], false, true );
 
-	add_editor_style( AssetResolver::resolve( 'css/editor.css' ) );
-
 	// enqueue global assets
 	wp_enqueue_script( 'jquery' );
 	wp_enqueue_style( 'app' );
 	wp_enqueue_script( 'app' );
 
 } );
+
+
+/**
+ * Registers an editor stylesheet for the theme.
+ */
+function wpdocs_theme_add_editor_styles() {
+    add_editor_style( AssetResolver::resolve( 'css/editor.css' ) );
+}
+add_action( 'admin_init', 'wpdocs_theme_add_editor_styles' );
