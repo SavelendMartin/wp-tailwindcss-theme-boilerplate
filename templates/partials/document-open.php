466054e583f3eb43fc0d53e4e0bd6671f8db261e
Martin Dahlén Jansson <martin@savelend.se>
2021-05-24 09:56:20 +0200

    Update document-open.php

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="SaveLend Group – Push For Change.">
+    <meta property="og:description" content="SaveLend Group utmanar de etablerade finansiella aktörerna, som banker och andra finansiella institut, framförallt genom innovativa produkter men också genom tillgänglighet och pris.">
     <meta property="og:image" content="https://savelendgroup.se/wp-content/uploads/2021/05/savelend_group_og_image.jpg" />
     <title><?= wp_get_document_title() ?></title>
 
