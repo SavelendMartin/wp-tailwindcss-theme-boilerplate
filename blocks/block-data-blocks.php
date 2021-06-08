12bc89a8e7b2350034019d84c70545d3e94cba8b
Martin Dahlén Jansson <martin@savelend.se>
2021-06-08 23:43:05 +0200

    Update block-data-blocks.php

--- blocks/block-data-blocks.php
+++ blocks/block-data-blocks.php
@@ -3,6 +3,10 @@
 $stockTableRaw = callAPI('GET', 'https://api2.savelend.se/datablocks/stock-table', ['c'=>'bbc48482-1063-43c7-8172-49c79a3072a5', 'token'=>'dd69f9bf-5450-4682-8b0e-8e970ec32bdb', 'demo'=>true]);
 $stockTable = json_decode($stockTableRaw);
 
+$ownerListRaw = callAPI('GET', 'https://api2.savelend.se/datablocks/owner-list', ['c'=>'bbc48482-1063-43c7-8172-49c79a3072a5', 'token'=>'aa44b943-71ce-460f-81d9-784f9f53abce', 'demo'=>true]);
+$ownerList = json_decode($stockTableRaw);
+
+print_r($ownerList);
 
 function formatMoney($n) {
     $n = (0+str_replace(",","",$n));
