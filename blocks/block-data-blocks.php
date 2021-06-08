21796baead3f95f48631fe5bd9e72994340a733d
Martin Dahlén Jansson <martin@savelend.se>
2021-06-08 23:25:14 +0200

    Update block-data-blocks.php

--- blocks/block-data-blocks.php
+++ blocks/block-data-blocks.php
@@ -3,8 +3,6 @@
 $stockTableRaw = callAPI('GET', 'https://api2.savelend.se/datablocks/stock-table', ['c'=>'bbc48482-1063-43c7-8172-49c79a3072a5', 'token'=>'dd69f9bf-5450-4682-8b0e-8e970ec32bdb', 'demo'=>true]);
 $stockTable = json_decode($stockTableRaw);
 
-print_r($stockTable);
-
 ?>
 
 <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto my-16 px-4">
