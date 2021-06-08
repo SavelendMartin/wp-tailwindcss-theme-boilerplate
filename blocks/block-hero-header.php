0865b3ffacf899d2741c418693fcbed74c23bc20
Martin Dahlén Jansson <martin@savelend.se>
2021-06-08 22:42:58 +0200

    updates

---
+++ blocks/block-hero-header.php
@@ -0,0 +1,5 @@
+<div class="bg-offwhite flex justify-center items-center flex-col">
+    <div class="py-24 px-2">
+        <img src="">
+    </div>
+</div>
--- functions.php
+++ functions.php
@@ -1,10 +1,5 @@
 <?php
 
-
-use SavelendGroupTheme\ AutoLoader;
-use SavelendGroupTheme\ View;
-
-
 /*
  * Set up our auto loading class and mapping our namespace to the app directory.
  *
@@ -14,17 +9,17 @@ use SavelendGroupTheme\ View;
  * i.e; If a class named SomeClass is stored in app/SomeDir/SomeClass.php, there is no need to include/require that file
  * as the autoloader will handle that for you.
  */
-require get_stylesheet_directory().'/app/AutoLoader.php';
+require get_stylesheet_directory() . '/app/AutoLoader.php';
 $loader = new AutoLoader();
-$loader -> register();
-$loader -> addNamespace('SavelendGroupTheme', get_stylesheet_directory().'/app');
+$loader->register();
+$loader->addNamespace('SavelendGroupTheme', get_stylesheet_directory() . '/app');
 
-View::$view_dir = get_stylesheet_directory().'/templates/views';
+View::$view_dir = get_stylesheet_directory() . '/templates/views';
 
-require get_stylesheet_directory().'/includes/scripts-and-styles.php';
+require get_stylesheet_directory() . '/includes/scripts-and-styles.php';
 
-
-function register_custom_widget_areas() {
+function register_custom_widget_areas()
+{
     register_sidebar(
         array(
             'id' => 'footer-area-1',
@@ -33,7 +28,7 @@ function register_custom_widget_areas() {
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<div class="widget-title-holder"><h3 class="widget-title">',
-            'after_title' => '</h3></div>'
+            'after_title' => '</h3></div>',
         )
     );
 
@@ -45,7 +40,7 @@ function register_custom_widget_areas() {
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<div class="widget-title-holder"><h3 class="widget-title">',
-            'after_title' => '</h3></div>'
+            'after_title' => '</h3></div>',
         )
     );
 
@@ -57,7 +52,7 @@ function register_custom_widget_areas() {
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<div class="widget-title-holder"><h3 class="widget-title">',
-            'after_title' => '</h3></div>'
+            'after_title' => '</h3></div>',
         )
     );
 
@@ -69,8 +64,49 @@ function register_custom_widget_areas() {
             'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
             'before_title' => '<div class="widget-title-holder"><h3 class="widget-title">',
-            'after_title' => '</h3></div>'
+            'after_title' => '</h3></div>',
         )
     );
 }
 add_action('widgets_init', 'register_custom_widget_areas');
+
+
+
+function callAPI($method, $url, $data)
+{
+    $curl = curl_init();
+    switch ($method) {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data) {
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            }
+
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if ($data) {
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            }
+
+            break;
+        default:
+            if ($data) {
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+            }
+
+    }
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'Authorization: Bearer 1|Go2hefbVoXwsnme8TCsMVS9Idxmiu3FbJ5Hd0O78',
+        'Content-Type: application/json',
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if (!$result) {die("Connection Failure");}
+    curl_close($curl);
+    return $result;
+}
